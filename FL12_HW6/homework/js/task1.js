44f1807e04c712a72d907179bc712c12c40cb7a2
Hanna Pereverzieva <41108545+annyrandie@users.noreply.github.com>
2019-12-23 20:05:51 +0200

    Update task1.js

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,24 +1,30 @@
 // Your code goes here
-let a = +prompt("Введите значение a", "");
-let b = +prompt("Введите значение b", "");
-let c = +prompt("Введите значение c", "");
-let x;
-let x1;
-let x2;
+let a = prompt("Введите значение a", "");
+let b = prompt("Введите значение b", "");
+let c = prompt("Введите значение c", "");
 
-if (isNaN(a)||isNaN(b)||isNaN(c)) {
-    console.log("Invalid input data");
-};
-
-if (Math.pow(b,2) - 4*a*c ==0) {
-    console.log(x = Math.round((-b + Math.sqrt(Math.pow(b,2) - 4*a*c))/(2*a)));
-} else if(Math.pow(b,2) - 4*a*c > 0) {
-    console.log(x1 = Math.round((-b + Math.sqrt(Math.pow(b,2) - 4*a*c))/(2*a)));
-    console.log(x2 = Math.round((-b - Math.sqrt(Math.pow(b,2) - 4*a*c))/(2*a)));
+if (a.trim().length == 0 || b.trim().length == 0 || c.trim().length == 0) {
+    alert('Invalid input data');  
 }
 else {
-    console.log("no solution");
+    a = Number(a);
+    b = Number(b);
+    c = Number(c);
+if ( isNaN(a) || isNaN(b) || isNaN(c) || a== 0) {
+    alert('Invalid input data');
+} else {
+    if (Math.pow(b,2) - 4*a*c ==0) {
+        console.log(`x = ${Math.round((-b + Math.sqrt(Math.pow(b,2) - 4*a*c))/(2*a))}`);
+    } else if(Math.pow(b,2) - 4*a*c > 0) {
+        console.log(`x1 = ${Math.round((-b + Math.sqrt(Math.pow(b,2) - 4*a*c))/(2*a))}`);
+        console.log(`x2 = ${Math.round((-b - Math.sqrt(Math.pow(b,2) - 4*a*c))/(2*a))}`);
+    }
+    else {
+        console.log("no solution");
+    }
 }
+}
+
 
 
 

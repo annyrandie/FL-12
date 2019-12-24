6f4d836d9a35a7095ba0cf7bf162e1f7e297e4c2
Hanna Pereverzieva <41108545+annyrandie@users.noreply.github.com>
2019-12-24 12:51:08 +0200

    fix according to eslint

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,30 +1,27 @@
 // Your code goes here
+let a = prompt('Введите значение a', '');
+let b = prompt('Введите значение b', '');
+let c = prompt('Введите значение c', '');
 
-let a = prompt("Введите значение a", "");
-let b = prompt("Введите значение b", "");
-let c = prompt("Введите значение c", "");
-
-if (a.trim().length == 0 || b.trim().length == 0 || c.trim().length == 0) {
-    alert("input values should be ONLY numbers" );  
-}
-else {
+if (a.trim().length === 0 || b.trim().length === 0 || c.trim().length === 0) {
+    alert('input values should be ONLY numbers');  
+} else {
     a = Number(a);
     b = Number(b);
     c = Number(c);
 if ( isNaN(a) || isNaN(b) || isNaN(c) ) {
-    alert("input values should be ONLY numbers" );
+    alert('input values should be ONLY numbers');
 } else {
-    if (a==0 || b == 0 || c == 0) {
+    if (a===0 || b ===0 || c === 0) {
         alert('A triangle must have 3 sides with a positive definite length');
     } else if 
         (a >= b + c || b >= a + c || c >= a + b) {
             alert('Triangle doesn’t exist');
-        }
-        else if 
-        (a ==b && b==c && a==c) {
+        } else if 
+        (a ===b && b===c && a===c) {
             alert('Equilateral triangle');
         } else if 
-        (a == b && b!==c || b==c && a !== c) {
+        (a === b && b!==c || b===c && a !== c) {
             alert('Isosceles triangle');
         } else if 
         (a !==b && a!==c && b!==c) {

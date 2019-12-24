9b80b65a019a4bb93f9d855af683aff57ae7fa46
Hanna Pereverzieva <41108545+annyrandie@users.noreply.github.com>
2019-12-24 12:49:35 +0200

    fix according to eslint

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -3,28 +3,22 @@ let a = prompt("Введите значение a", "");
 let b = prompt("Введите значение b", "");
 let c = prompt("Введите значение c", "");
 
-if (a.trim().length == 0 || b.trim().length == 0 || c.trim().length == 0) {
-    alert('Invalid input data');  
-}
-else {
+if (a.trim().length === 0 || b.trim().length === 0 || c.trim().length === 0) {
+    alert('Invalid input data');
+} else {
     a = Number(a);
     b = Number(b);
     c = Number(c);
-if ( isNaN(a) || isNaN(b) || isNaN(c) || a== 0) {
+if ( isNaN(a) || isNaN(b) || isNaN(c) || a=== 0) {
     alert('Invalid input data');
 } else {
-    if (Math.pow(b,2) - 4*a*c ==0) {
+    if (Math.pow(b,2) - 4*a*c ===0) {
         console.log(`x = ${Math.round((-b + Math.sqrt(Math.pow(b,2) - 4*a*c))/(2*a))}`);
     } else if(Math.pow(b,2) - 4*a*c > 0) {
         console.log(`x1 = ${Math.round((-b + Math.sqrt(Math.pow(b,2) - 4*a*c))/(2*a))}`);
         console.log(`x2 = ${Math.round((-b - Math.sqrt(Math.pow(b,2) - 4*a*c))/(2*a))}`);
-    }
-    else {
-        console.log("no solution");
+    } else {
+        console.log('no solution');
     }
 }
 }
-
-
-
-

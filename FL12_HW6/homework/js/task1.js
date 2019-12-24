1d12a21431cef8f3542091414a838544d926fff6
Hanna Pereverzieva <41108545+annyrandie@users.noreply.github.com>
2019-12-24 21:44:26 +0200

    Update task1.js

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,7 +1,7 @@
 // Your code goes here
-let a = prompt("Введите значение a", "");
-let b = prompt("Введите значение b", "");
-let c = prompt("Введите значение c", "");
+let a = prompt('Введите значение a', '');
+let b = prompt('Введите значение b', '');
+let c = prompt('Введите значение c', '');
 const DISCR_POWER_COEF = 2;
 const DISC_MULT_COEF = 4;
 const DENOMINATOR_MULT = 2;
@@ -16,12 +16,18 @@ if ( isNaN(a) || isNaN(b) || isNaN(c) || a=== 0) {
     alert('Invalid input data');
 } else {
     if (Math.pow(b, DISCR_POWER_COEF) - DISC_MULT_COEF*a*c ===0) {
-        console.log(`x = ${Math.round(((-1)*b + Math.sqrt(Math.pow(b,DISCR_POWER_COEF) - DISC_MULT_COEF*a*c))/(DENOMINATOR_MULT*a))}`);
+        console.log(`x = ${Math.round((-b + Math.sqrt(Math.pow(b,DISCR_POWER_COEF) 
+            - DISC_MULT_COEF*a*c))/(DENOMINATOR_MULT*a))}`);
     } else if(Math.pow(b,DISCR_POWER_COEF) - DISC_MULT_COEF*a*c > 0) {
-        console.log(`x1 = ${Math.round(((-1)*b + Math.sqrt(Math.pow(b,DISCR_POWER_COEF) - DISC_MULT_COEF*a*c))/(DENOMINATOR_MULT*a))}`);
-        console.log(`x2 = ${Math.round(((-1)*b - Math.sqrt(Math.pow(b,DISCR_POWER_COEF) - DISC_MULT_COEF*a*c))/(DENOMINATOR_MULT*a))}`);
+        console.log(`x1 = ${Math.round((-b + Math.sqrt(Math.pow(b,DISCR_POWER_COEF) 
+            - DISC_MULT_COEF*a*c))/(DENOMINATOR_MULT*a))}`);
+        console.log(`x2 = ${Math.round((-b - Math.sqrt(Math.pow(b,DISCR_POWER_COEF) 
+            - DISC_MULT_COEF*a*c))/(DENOMINATOR_MULT*a))}`);
     } else {
         console.log('no solution');
     }
 }
 }
+
+
+

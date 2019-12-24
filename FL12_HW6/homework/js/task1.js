ae19040e5f4a8b3062bd0679b6aa587f3d7da6ec
Hanna Pereverzieva <41108545+annyrandie@users.noreply.github.com>
2019-12-24 17:16:11 +0200

    fix magic numbers

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -2,6 +2,9 @@
 let a = prompt("Введите значение a", "");
 let b = prompt("Введите значение b", "");
 let c = prompt("Введите значение c", "");
+const DISCR_POWER_COEF = 2;
+const DISC_MULT_COEF = 4;
+const DENOMINATOR_MULT = 2;
 
 if (a.trim().length === 0 || b.trim().length === 0 || c.trim().length === 0) {
     alert('Invalid input data');
@@ -12,11 +15,11 @@ if (a.trim().length === 0 || b.trim().length === 0 || c.trim().length === 0) {
 if ( isNaN(a) || isNaN(b) || isNaN(c) || a=== 0) {
     alert('Invalid input data');
 } else {
-    if (Math.pow(b,2) - 4*a*c ===0) {
-        console.log(`x = ${Math.round((-b + Math.sqrt(Math.pow(b,2) - 4*a*c))/(2*a))}`);
-    } else if(Math.pow(b,2) - 4*a*c > 0) {
-        console.log(`x1 = ${Math.round((-b + Math.sqrt(Math.pow(b,2) - 4*a*c))/(2*a))}`);
-        console.log(`x2 = ${Math.round((-b - Math.sqrt(Math.pow(b,2) - 4*a*c))/(2*a))}`);
+    if (Math.pow(b, DISCR_POWER_COEF) - DISC_MULT_COEF*a*c ===0) {
+        console.log(`x = ${Math.round(((-1)*b + Math.sqrt(Math.pow(b,DISCR_POWER_COEF) - DISC_MULT_COEF*a*c))/(DENOMINATOR_MULT*a))}`);
+    } else if(Math.pow(b,DISCR_POWER_COEF) - DISC_MULT_COEF*a*c > 0) {
+        console.log(`x1 = ${Math.round(((-1)*b + Math.sqrt(Math.pow(b,DISCR_POWER_COEF) - DISC_MULT_COEF*a*c))/(DENOMINATOR_MULT*a))}`);
+        console.log(`x2 = ${Math.round(((-1)*b - Math.sqrt(Math.pow(b,DISCR_POWER_COEF) - DISC_MULT_COEF*a*c))/(DENOMINATOR_MULT*a))}`);
     } else {
         console.log('no solution');
     }
